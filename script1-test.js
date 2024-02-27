c406993f47270974f345d6b888e578791437f9ba
Marjus18 <149163902+Marjus18@users.noreply.github.com>
2024-02-27 18:35:41 +0100

    update2

---
+++ script1-test.js
@@ -0,0 +1,12 @@
+// function script1(params) {
+//   console.log("do blesh cant e qen?");
+//   let numri1 = +prompt("numri i kartes se kreditit");
+//   let numri2 = +prompt("data e skadimit te kartes");
+//   let nr3 = +prompt("sa lek ben produkti?");
+//   let nr4 = +prompt("sa lek ke tek karta jote?...mos genje!");
+//   if (nr4 > nr3) {
+//     console.log("u konfirmua,ta gzoni");
+//   } else {
+//     alert("ske lek po ik andej!");
+//   }
+// }
